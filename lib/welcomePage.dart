48ef34c9ece7217ecbbdb7e62bc4da5e36cf1de5
jocscriptch <jocsanchaves20@gmail.com>
2024-03-02 19:09:21 -0600

    facebook-login

--- lib/welcomePage.dart
+++ lib/welcomePage.dart
@@ -2,9 +2,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+<<<<<<< HEAD:lib/welcomePage.dart
 import 'package:trabajorapid/regPage.dart';
 import 'loginPage.dart';
 import 'package:trabajorapid/moduleMain.dart';
+=======
+import 'package:trabajorapid/components/register/regPage.dart';
+import 'package:trabajorapid/components/login/loginPage.dart';
+import 'package:trabajorapid/mainHome/moduleMain.dart';
+import 'package:flutter_login_facebook/flutter_login_facebook.dart';
+
+>>>>>>> 2bb41e6 (facebook-login):lib/components/welcomeLogin/welcomePage.dart
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -123,7 +131,7 @@ class WelcomePage extends StatelessWidget {
                 children: [
                   IconButton(
                     onPressed: () {
-                      // Acción cuando se presiona el botón de Facebook
+                      signInWithFacebook();
                     },
                     icon: Image.asset('assets/images/facebook.png', height: 50),
                   ),
@@ -177,6 +185,48 @@ class WelcomePage extends StatelessWidget {
       }
     } catch (e) {
       print('Error signing in with Google: $e');
+    }
+  }
+
+  signInWithFacebook() async {
+    final FacebookLogin fb = FacebookLogin();
+
+    try {
+      final FacebookLoginResult result = await fb.logIn(permissions: [
+        FacebookPermission.publicProfile,
+        FacebookPermission.email,
+      ]);
+
+      switch (result.status) {
+        case FacebookLoginStatus.success:
+          final FacebookAccessToken accessToken = result.accessToken!;
+          print('Access token: ${accessToken.token}');
+
+          final AuthCredential credential = FacebookAuthProvider.credential(
+            accessToken.token,
+          );
+
+          final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+          // Verifica si la autenticación con Firebase fue exitosa
+          if (userCredential.user != null) {
+            // Navega a ModuleMain() después del inicio de sesión exitoso
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const ModuleMain()),
+            );
+          }
+          break;
+        case FacebookLoginStatus.cancel:
+          print('El inicio de sesión con Facebook fue cancelado por el usuario');
+          break;
+        case FacebookLoginStatus.error:
+          print('Error al iniciar sesión con Facebook: ${result.error}');
+          break;
+      }
+    } catch (e) {
+      print('Error al iniciar sesión con Facebook: $e');
     }
   }
 }
