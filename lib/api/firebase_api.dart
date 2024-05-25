697321ee3c3887d1c979320f2697afb10d55d865
Jordan Laguna <jordanlaguna10@gmail.com>
2024-05-24 21:53:35 -0600

    send notifications push

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,45 +1,30 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:googleapis_auth/auth_io.dart';
 import 'dart:convert';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:http/http.dart' as http;
 
 class FirebaseApi {
-  // create an instance of firebase messaging
   final _firebaseMessaging = FirebaseMessaging.instance;
-  final String _serverKey = 'AIzaSyCRtbvKx1iBuh6lKmLLxHerpP_iU1mvg74';
 
-  // function to initialize notifications
   Future<void> initNotifications() async {
-    // request for permission
     await _firebaseMessaging.requestPermission();
-
-    // fetch the token from the firebase messaging
     final fCMToken = await _firebaseMessaging.getToken();
-
-    // print the token
     print('FCM Token: $fCMToken');
   }
 
-  // function to handle received messages
   void handleMessage(RemoteMessage? message) {
-    // check if the message is null
-    if (message == null) {
-      return;
-    }
-    // print the message data
+    if (message == null) return;
     print('Message data: ${message.data}');
-    // print the message notification
     print('Message notification: ${message.notification}');
   }
 
-  // function to initialize foreground and background settings
-  Future initPushNotification() async {
+  Future<void> initPushNotification() async {
     FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
-
     FirebaseMessaging.onMessage.listen(handleMessage);
   }
 
-  // function to send push notifications
   Future<void> sendPushNotification(
       String receiverId, String senderName, String message) async {
     final tokenSnapshot = await FirebaseFirestore.instance
@@ -52,11 +37,13 @@ class FirebaseApi {
       final payload = constructFCMPayload(token, senderName, message);
 
       try {
+        final accessToken = await _getAccessToken();
         await http.post(
-          Uri.parse('https://fcm.googleapis.com/fcm/send'),
+          Uri.parse(
+              'https://fcm.googleapis.com/v1/projects/trabajosrapid-f63a2/messages:send'),
           headers: <String, String>{
             'Content-Type': 'application/json; charset=UTF-8',
-            'Authorization': 'key=$_serverKey',
+            'Authorization': 'Bearer $accessToken',
           },
           body: payload,
         );
@@ -69,14 +56,35 @@ class FirebaseApi {
 
   String constructFCMPayload(String token, String senderName, String message) {
     return jsonEncode({
-      'to': token,
-      'data': {
-        'via': 'FlutterFire Cloud Messaging!!!',
-      },
-      'notification': {
-        'title': 'Nuevo mensaje de $senderName',
-        'body': message,
+      'message': {
+        'token': token,
+        'notification': {
+          'title': 'Nuevo mensaje de $senderName',
+          'body': message,
+        },
+        'data': {
+          'via': 'FlutterFire Cloud Messaging!!!',
+        },
       },
     });
+  }
+
+  Future<String> _getAccessToken() async {
+    const serviceAccountPath = 'assets/service_account_file.json';
+    try {
+      final serviceAccountJson =
+          await rootBundle.loadString(serviceAccountPath);
+      final serviceAccountCredentials = ServiceAccountCredentials.fromJson(
+        json.decode(serviceAccountJson),
+      );
+      final authClient = await clientViaServiceAccount(
+        serviceAccountCredentials,
+        ['https://www.googleapis.com/auth/firebase.messaging'],
+      );
+      return authClient.credentials.accessToken.data;
+    } catch (e) {
+      print('Error reading service account file: $e');
+      rethrow;
+    }
   }
 }
