952304555c680e3df5bba4194a5f2f37d1d3ec4e
Antony Valverde R <gatovalverde4@gmail.com>
2024-06-01 00:38:24 -0600

    FiltroUidUsuarioEmisorORreceptor

--- lib/screens/bottom_navigationbar/work_page/works_services.dart
+++ lib/screens/bottom_navigationbar/work_page/works_services.dart
@@ -1,15 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:trabajorapid/screens/bottom_navigationbar/work_page/job.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseService {
   static Future<List<Job>> fetchJobs() async {
-    final trabajosSnapshot = await FirebaseFirestore.instance
-        .collection('trabajos')
-        .where('estado', whereIn: ['Cancelado','Pendiente', 'En proceso', 'Terminado']).get();
+    // Obtiene el uid del usuario actual
+    final String uid = FirebaseAuth.instance.currentUser!.uid;
 
-    final List<Job> jobs = trabajosSnapshot.docs.map((doc) {
-      return Job.fromFirestore(doc);
-    }).toList();
+    // Consulta para trabajos donde uidEmisor es igual al uid del usuario actual
+    final trabajosEmisorSnapshot = await FirebaseFirestore.instance
+        .collection('trabajos')
+        .where('estado',
+            whereIn: ['Cancelado', 'Pendiente', 'En proceso', 'Terminado'])
+        .where('uidEmisor', isEqualTo: uid)
+        .get();
+
+    // Consulta para trabajos donde uidReceptor es igual al uid del usuario actual
+    final trabajosReceptorSnapshot = await FirebaseFirestore.instance
+        .collection('trabajos')
+        .where('estado',
+            whereIn: ['Cancelado', 'Pendiente', 'En proceso', 'Terminado'])
+        .where('uidReceptor', isEqualTo: uid)
+        .get();
+
+    // Combina los resultados de ambas consultas
+    final List<Job> jobs = [
+      ...trabajosEmisorSnapshot.docs.map((doc) => Job.fromFirestore(doc)),
+      ...trabajosReceptorSnapshot.docs.map((doc) => Job.fromFirestore(doc)),
+    ];
 
     return jobs;
   }
